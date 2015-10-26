cd85de0699b63bcb624b5de09525eeaa6af12246
whatrocks <charles.harrington@gmail.com>
2015-10-26 15:02:42 -0700

    Got basic communication between client and server

---
+++ client/client/scripts/app.js
@@ -0,0 +1,122 @@
+/////////////////////////////////////////////////////////////////////////////
+// This file contains two implementations:
+//
+//    - a minimal jQuery solution
+//    - the refactor to Backbone
+//
+// A minimal jQuery solution is provided to make it easier to compare
+// it to the equivalent Backbone implementation. A minimal version of
+// chatterbox-client was used in order to reduce complexity, to make
+// the equivalent Backbone app easier to understand. This code will
+// be a useful reference as you continue to work with Backbone.
+//
+// Note: the file, refactor.html, should be used to launch these
+// version(s) of the app. It will run only one version -- by default it
+// runs the Backbone version.
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Backbone-based Implementation of (minimal) chatterbox client
+/////////////////////////////////////////////////////////////////////////////
+
+var Message = Backbone.Model.extend({
+  url: 'http://127.0.0.1:3000',
+  defaults: {
+    username: '',
+    text: ''
+  }
+});
+
+var Messages = Backbone.Collection.extend({
+
+  model: Message,
+  url: 'http://127.0.0.1:3000',
+
+  loadMsgs: function() {
+    this.fetch({data: { order: '-createdAt' }});
+  },
+
+  parse: function(response, options) {
+    var results = [];
+    for (var i = response.results.length-1; i >= 0; i--) {
+      results.push(response.results[i]);
+    }
+    return results;
+  }
+
+});
+
+var FormView = Backbone.View.extend({
+
+  initialize: function() {
+    this.collection.on('sync', this.stopSpinner, this);
+  },
+
+  events: {
+    'submit #send': 'handleSubmit'
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    this.startSpinner();
+
+    var $text = this.$('#message');
+    this.collection.create({
+      username: window.location.search.substr(10),
+      text: $text.val()
+    });
+    $text.val('');
+  },
+
+  startSpinner: function() {
+    this.$('.spinner img').show();
+    this.$('form input[type=submit]').attr('disabled', "true");
+  },
+
+  stopSpinner: function() {
+    this.$('.spinner img').fadeOut('fast');
+    this.$('form input[type=submit]').attr('disabled', null);
+  }
+
+});
+
+var MessageView = Backbone.View.extend({
+
+  initialize: function() {
+    this.model.on('change', this.render, this);
+  },
+
+  template: _.template('<div class="chat" data-id="<%- objectId %>"> \
+                          <div class="user"><%- username %></div> \
+                          <div class="text"><%- text %></div> \
+                        </div>'),
+
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
+  }
+
+});
+
+var MessagesView = Backbone.View.extend({
+
+  initialize: function() {
+    this.collection.on('sync', this.render, this);
+    this.onscreenMessages = {};
+  },
+
+  render: function() {
+    this.collection.forEach(this.renderMessage, this);
+  },
+
+  renderMessage: function(message) {
+    if (!this.onscreenMessages[message.get('objectId')]) {
+      var messageView = new MessageView({model: message});
+      this.$el.prepend(messageView.render());
+      this.onscreenMessages[message.get('objectId')] = true;
+    }
+  }
+
+});
