e314cf3228e06cfe954f185c400e2063feeb86b0
whatrocks <charles.harrington@gmail.com>
2015-10-27 12:32:56 -0700

    working on express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,39 +1,39 @@
 /* Import node's http module: */
-var http = require("http");
-var handleRequest = require("./request-handler.js")
+var express = require("express");
+var app = express();
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
-var port = 3000;
+// var handleRequest = require("./request-handler.js")
+var storage = {results: []}; 
+var objectId = 0;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = "127.0.0.1";
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+});
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest.requestHandler);
-	
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+var allowCrossDomain = function(req, res, next) {
+	res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("access-control-allow-headers", "content-type, accept");
+  res.header("access-control-max-age", 10);
+	next();
+};
+// var __dirname = "../client/client/";
+app.use(express.static(__dirname + "/client"));
+app.use(allowCrossDomain);
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/client/index.html');
+});
+
+app.get('/cloudmouth', function(req, res) {
+	res.send(storage);
+})
+
+app.post('/cloudmouth', function(req, res){
+	console.log(req.body);
+	req.body.objectId = ++objectId;
+	storage.results.push(req.body);
+	res.send('got a POST request');
+});
