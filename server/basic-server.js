26cd249a86830797de3cb97c7f9b148ac45ae5c1
whatrocks <charles.harrington@gmail.com>
2015-10-27 15:11:35 -0700

    Basic app functionality working with Express server

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,8 +1,9 @@
 /* Import node's http module: */
 var express = require("express");
+var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser');
 
-// var handleRequest = require("./request-handler.js")
 var storage = {results: []}; 
 var objectId = 0;
 
@@ -11,17 +12,9 @@ var server = app.listen(3000, function () {
 	var port = server.address().port;
 });
 
-
-var allowCrossDomain = function(req, res, next) {
-	res.header("access-control-allow-origin", "*");
-  res.header("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("access-control-allow-headers", "content-type, accept");
-  res.header("access-control-max-age", 10);
-	next();
-};
-// var __dirname = "../client/client/";
 app.use(express.static(__dirname + "/client"));
-app.use(allowCrossDomain);
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/client/index.html');
@@ -32,8 +25,9 @@ app.get('/cloudmouth', function(req, res) {
 })
 
 app.post('/cloudmouth', function(req, res){
-	console.log(req.body);
 	req.body.objectId = ++objectId;
+	req.body.createdAt = Date.now();
 	storage.results.push(req.body);
+	console.log(storage.results);
 	res.send('got a POST request');
 });
